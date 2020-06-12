948441d20f11543b6f841ee00f170a4297aaf1e5
fanuelnalem <fanuelnalem@outlook.com>
2020-06-11 23:15:21 -0700

    Fref

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-
-// import './index.css';
-ReactDOM.render(<App />,document.getElementById('root'));   
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
+import App from './containers/App';
+// import socketMiddleware from './reduxMiddlewares';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// 1st param is our reducers
+// 2nd param is any preloaded state we want
+// 3rd param is any middlwares we want applied to redux
+const store = createStore(
+  reducers,
+  { auth: { authenticated: localStorage.getItem('token')}},
+  composeEnhancers(applyMiddleware(reduxThunk))
+  // socketMiddleware(),
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  ,
+  document.getElementById('root')); 
