74e5d986188ff4ea068e153a89e171456e8aa9ce
fanuelnalem <fanuelnalem@outlook.com>
2020-06-11 22:45:25 -0700

    RFe

--- index.js
+++ index.js
@@ -1,55 +1,43 @@
-// const express = require('express');
-// const myroute = require('./routes/userRoutes/user')
-// const mysql = require('mysql')
-// const cors = require('cors')
-// // const sithRoutes = require('./routes/sithRoutes/sithRoutes')
-  
-// const PORT = process.env.PORT || 8000;
-
-// const app = express();
-
-
-// // if(process.env.NODE_ENV === 'production') {
-// //     app.use(express.static('./client/build'));
-// // }
-
-// app.use(cors())
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-
-// app.use("/" ,myroute)
-// // app.use("sith" ,sithRoutes)
-
-
-// // app.use("/sith",sithRoutes)
- 
- 
-
-
-// app.listen(PORT, () => console.log('Port started on port: ' + PORT));
-
 const express = require('express');
-const myroute = require('./routes/userRoutes/user')
-const mysql = require('mysql')
-const cors = require('cors')
- 
-const PORT = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+// const cors = require('cors');
+
 
 const app = express();
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
-}
-app.use(cors())
+// io.on('connection', socket => {
 
-app.use(express.json());
+//   socket.on('message', (data) => {
+//     socket.emit('serverToClientMessage', data);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log("See ya");
+//     return;
+//   });
+// });
+
+// Setup middlewares
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Declare routes right here.
- 
-// Prepend / to any route declared inside of routes
-app.use("/" ,myroute)
 
-app.listen(PORT, () => console.log('Port started on port: ' + PORT));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use("/",routes);
+require('./services/passport');
+// Connect database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/userstockitem', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+
+app.listen(PORT,()=>{
+  console.log(`listening on PORT ${PORT}`)
+});
