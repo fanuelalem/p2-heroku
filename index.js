f800b8ad5f24c8ceb6d35ff3397e1283cbc52fc8
fanuelnalem <fanuelnalem@outlook.com>
2020-06-12 04:51:04 -0700

    ferf

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const app = express();
 // const io = require('socket.io')(server);
 
 const routes = require('./routes');
-
-const PORT = process.env.PORT || 3001;
+ const PORT = process.env.PORT || 3001;
 
 
 // io.on('connection', socket => {
@@ -32,6 +31,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.use("/",routes);
 require('./services/passport');
